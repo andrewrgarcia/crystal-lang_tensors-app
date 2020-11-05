08dc0c6e9b4fe7477580705543ffb589e88f1166
andrewrgarcia <andrewrgarcia@ufl.edu>
2020-11-05 17:38:51 -0500

    changes firstapp.cr

--- src/firstapp.cr
+++ src/firstapp.cr
@@ -4,21 +4,20 @@ require "num"
 module Firstapp
 	VERSION = "0.1.0"
 	
-
+	a = [1, 2, 3].to_tensor
 	b = Tensor.from_array [1, 2, 3]
 	c = Tensor(UInt8).zeros([3, 3, 2])
 	d = Tensor.random(0.0...1.0, [2, 2, 2])
 	
-	#puts a; puts; puts b; puts; puts c; puts; puts d
+	puts a; puts; puts b; puts; puts c; puts; puts d
 	
 	#c = Tensor(UInt8).zeros([1000,1000, 1000])
 	#puts c
 	
-	a = [1, 2, 3].to_tensor
 	#some_tensor = a 
 	#ClTensor(Float32).zeros_like(some_tensor)
 	
-	puts ClTensor(Float64).full([3, 4, 5], 3.8)
+	#puts ClTensor(Float64).full([3, 4, 5], 3.8)
 	
 	# TODO: Put your code here
 end
